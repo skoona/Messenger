9c1bac995725e1f7046c989a586a8a6f72338b77
James Scott Jr <skoona@gmail.com>
2023-08-03 17:24:54 -0400

    Initial commit of Hexagonal Architecture Module

---
+++ cmd/main.go
@@ -0,0 +1,45 @@
+package main
+
+// ref: https://www.golinuxcloud.com/hexagonal-architectural-golang/
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"Messenger/internal/adapters/handler"
+	"Messenger/internal/adapters/repository"
+	"Messenger/internal/core/services"
+)
+
+var (
+	repo        = flag.String("db", "postgres", "Database for storing messages")
+	redisHost   = "localhost:6379"
+	httpHandler *handler.HTTPHandler
+	svc         *services.MessengerService
+)
+
+func main() {
+	flag.Parse()
+
+	fmt.Printf("Application running using %s\n", *repo)
+	switch *repo {
+	case "redis":
+		store := repository.NewMessengerRedisRepository(redisHost)
+		svc = services.NewMessengerService(store)
+	default:
+		store := repository.NewMessengerPostgresRepository()
+		svc = services.NewMessengerService(store)
+	}
+
+	InitRoutes()
+
+}
+
+func InitRoutes() {
+	router := gin.Default()
+	handler := handler.NewHTTPHandler(*svc)
+	router.GET("/messages/:id", handler.ReadMessage)
+	router.GET("/messages", handler.ReadMessages)
+	router.POST("/messages", handler.SaveMessage)
+	router.Run(":3000")
+}
